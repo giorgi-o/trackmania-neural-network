772d8eaca03a06a496896792df9a78b81961d69e
Aaron <aaron.kher@gmail.com>
2023-12-21 17:26:46 +0000

    DQN progress

--- dqn.py
+++ dqn.py
@@ -7,27 +7,64 @@ from network import NeuralNetwork
 
 class DQN:
 
-    def __init__(self, episode_count: int, epsilon: float):
+    def __init__(
+            self,
+            episode_count: int,
+            timestep_count: int,
+            epsilon: float,
+            gamma: float
+        ):
         self.episode_count = episode_count
+        self.timestep_count = timestep_count
         self.epsilon = epsilon
+        self.gamma = gamma
 
         self.environment = Environment()
         self.neural_network = NeuralNetwork()
 
-    def get_action(self):
+    def get_best_action(self, state: State) -> Action:
+            return self.neural_network.best_action(state)
+
+    def get_action(self, state: State):
         if np.random.uniform(0, 1) < self.epsilon:
             # pick random action
             action = random.choice(self.environment.action_list())
         else:
             # pick best action
-            action = self.neural_network.best_action(self.environment.current_state)
+            action = self.get_best_action(state)
         return action
 
     def execute_action(self, action: Action) -> ActionTaken:
         return self.environment.take_action(action)
 
+    def get_q_value(self, state: State):
+        action = self.get_best_action(state)
+        # get logits (q values) from neural network 
+        q_value = self.neural_network(state)[action]
+
+        
+
+        return q_value
+
+    def compute_td_target(self, terminal_status: bool, action: Action):
+        last_reward = self.environment.last_reward()
+        current_state = self.environment.current_state()
+        if terminal_status:
+            td_target = last_reward
+        else:
+            td_target = last_reward + self.gamma * self.get_q_value(current_state)
+        
+    def gradient_descent(self):
+        self.neural_network.gradient_descent()
 
     def train_dqn(self):
-        for episode in range(self.episodes):
-            action = self.get_action()
+        for episode in range(self.episode_count):
+            self.environment.reset()
+            
+            for timestep in range(self.timestep_count):              
+                action = self.get_action()
+                self.execute_action(action)
+                
+                yt = self.compute_td_target()
 
+                self.gradient_descent()
--- environment.py
+++ environment.py
@@ -20,31 +20,52 @@ class Environment:
     def __init__(self):
         self.env = gymnasium.make('Acrobot-v1', render_mode="human")
         
-        (current_state, _) = self.env.reset()
-        self.current_state = current_state
+        self.reset()
+        self.last_action_taken: ActionTaken
 
+    @property
     def action_list(self) -> list[Action]:
         # todo replace with self.env.action_space.something
         return [0, 1, 2]
 
+    @property
     def action_count(self) -> int:
         len(self.action_space())
 
+    @property
     def observation_space_length(self) -> int:
         # todo replace with self.env.observation_space.something
         return 6
     
-    def take_action(self, action: Action) -> ActionTaken:
+    def take_action(self, action: Action):
         old_state = self.current_state
         (new_state, reward, terminated, truncated, info) = self.env.step(action)
 
         self.current_state = new_state
-        return ActionTaken(action, old_state, new_state, reward, terminated)
+        self.last_action_taken = ActionTaken(action, old_state, new_state, reward, terminated)
 
     def reset(self):
         (current_state, _) = self.env.reset()
-        self.current_state = current_state
+        self.last_action_taken = ActionTaken(
+            action=None,
+            old_state=None,
+            new_state=current_state,
+            reward=0.0,
+            terminated=False
+        )
+
+    @property
+    def current_state(self) -> State:
+        self.last_action_taken.new_state
+
+    @property
+    def is_terminated(self) -> bool:
+        self.last_action_taken.terminated
     
+    @property
+    def last_reward(self) -> float:
+        self.last_action_taken.reward
+
     def render(self):
         self.env.render()
 
