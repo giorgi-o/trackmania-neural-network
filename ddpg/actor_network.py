f969c16dfad69f1c862c95ef14988fff8711fa0d
TomChristensen474 <christensentom9@gmail.com>
2024-01-02 00:53:00 +0000

    implementing actor network

--- ddpg/actor_network.py
+++ ddpg/actor_network.py
@@ -1,7 +1,8 @@
 import torch
-from environment import Environment
+from environment import Environment, State, Action
 from network import NeuralNetwork
 from replay_buffer import TransitionBatch
+from critic_network import CriticNetwork
 
 
 class ActorNetwork(NeuralNetwork):
@@ -16,10 +17,32 @@ class ActorNetwork(NeuralNetwork):
         copy = ActorNetwork(self.environment)
         copy.copy_from(self)
         return copy
+    
+    def get_action(self, state: State) -> Action:
+        output = self(torch.Tensor(state))
+        return output
 
     def get_actions(self, states: torch.Tensor) -> torch.Tensor:
         output = self(states)
         return output
 
-    def train(self, experiences: TransitionBatch):
-        pass
+    def train(self, experiences: TransitionBatch, critic_network: CriticNetwork):
+        # Tensor[QValue, QValue, ...]
+        q_values = critic_network.get_q_values(
+            experiences.old_states,
+            self.get_action_batch(experiences.old_states)
+            )
+
+        self.gradient_ascent(q_values)
+
+    def gradient_ascent(self, q_values: torch.Tensor):
+        self.optim.zero_grad()
+
+        loss = -q_values.mean()
+        loss.backward()
+
+        self.optim.step()
+
+    def update_target_weights(self, network_to_copy: NeuralNetwork, target_network_learning_rate: float):
+        for target_param, param in zip(self.linear_relu_stack.parameters(), network_to_copy.parameters()): # TODO rename linear_relu_stack to "network" or eq.
+            target_param.data.copy_(param.data * target_network_learning_rate + target_param.data * (1.0 - target_network_learning_rate))
