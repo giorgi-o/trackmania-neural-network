491e09cea76580a7e19b306980ccfdd08c066f03
TomChristensen474 <82264960+TomChristensen474@users.noreply.github.com>
2024-01-05 19:41:29 +0000

    if loading from checkpoint don't test x new agents

--- main.py
+++ main.py
@@ -20,14 +20,12 @@ if __name__ == "__main__":
     args = parse_args()
     checkpoint_id: str | None = args.checkpoint_id
     epsilon_start: float | None = args.epsilon_start
-    high_score = 0
-    best_agent: DQN | None = None
 
-    for i in range(9):
-        print(f"Training agent: {i+1}\n")
+    if checkpoint_id is not None:
+        print(f"Loading agent from checkpoint: {checkpoint_id}\n")
         agent = DQN(
             environment=env,
-            episode_count=5,
+            episode_count=10 * 1000,
             timestep_count=10 * 1000,
             gamma=0.99,
             epsilon_start=args.epsilon_start or 0.5,
@@ -36,31 +34,53 @@ if __name__ == "__main__":
             buffer_batch_size=256,
             checkpoint_id=checkpoint_id,
         )
-
         agent.train()
-        # ddpg = DDPG(
-        #     episode_count=600,
-        #     gamma=0.99,
-        #     buffer_batch_size=128,
-        #     target_network_learning_rate=0.005,
-        # )
-        # ddpg.train()
-        if agent.high_score > high_score:
-            print(f"Starting agent upgraded, new highest score: {agent.high_score}\n")
-            best_agent = agent
-            high_score = agent.high_score
+        sys.exit(0)
+    else:
+        print("No checkpoint id provided, training new agent\n")
+        high_score = 0
+        best_agent: DQN | None = None
 
-    assert best_agent is not None
-    print("Training best agent \n")
-    best_agent_continued = DQN(
-        environment=env,
-        episode_count=10 * 1000,
-        timestep_count=10 * 1000,
-        gamma=0.99,
-        epsilon_start=args.epsilon_start or 0.5,
-        epsilon_min=0.01,
-        epsilon_decay=0.01,
-        buffer_batch_size=256,
-        checkpoint_id=best_agent.latest_checkpoint,
-    )
-    best_agent_continued.train()
+        for i in range(9):
+            print(f"Training agent: {i+1}\n")
+            agent = DQN(
+                environment=env,
+                episode_count=5,
+                timestep_count=10 * 1000,
+                gamma=0.99,
+                epsilon_start=args.epsilon_start or 0.5,
+                epsilon_min=0.01,
+                epsilon_decay=0.01,
+                buffer_batch_size=256,
+                checkpoint_id=checkpoint_id,
+            )
+
+            agent.train()
+            # ddpg = DDPG(
+            #     episode_count=600,
+            #     gamma=0.99,
+            #     buffer_batch_size=128,
+            #     target_network_learning_rate=0.005,
+            # )
+            # ddpg.train()
+            if agent.high_score > high_score:
+                print(
+                    f"Starting agent upgraded, new highest score: {agent.high_score}\n"
+                )
+                best_agent = agent
+                high_score = agent.high_score
+
+        assert best_agent is not None
+        print("Training best agent \n")
+        best_agent_continued = DQN(
+            environment=env,
+            episode_count=10 * 1000,
+            timestep_count=10 * 1000,
+            gamma=0.99,
+            epsilon_start=args.epsilon_start or 0.5,
+            epsilon_min=0.01,
+            epsilon_decay=0.01,
+            buffer_batch_size=256,
+            checkpoint_id=best_agent.latest_checkpoint,
+        )
+        best_agent_continued.train()
